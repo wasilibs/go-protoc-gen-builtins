a5fd7328fb7ecbefb43b81c64c252db7fc856260
Anuraag Agrawal <anuraaga@gmail.com>
2023-12-21 12:54:05 +0900

    Add release workflow

--- go.mod
+++ go.mod
@@ -1,5 +1,5 @@
 module github.com/wasilibs/go-protoc-gen-builtins
 
-go 1.19
+go 1.21
 
 require github.com/wasilibs/wazerox v0.0.0-20231215071156-a88739a1af2a
--- magefiles/magefile.go
+++ magefiles/magefile.go
@@ -1,9 +1,21 @@
 package main
 
 import (
+	"fmt"
+
+	"github.com/magefile/mage/sh"
+
 	"github.com/wasilibs/magefiles" // mage:import
 )
 
 func init() {
 	magefiles.SetLibraryName("protoc")
+}
+
+func Snapshot() error {
+	return sh.RunV("go", "run", fmt.Sprintf("github.com/goreleaser/goreleaser@%s", verGoReleaser), "release", "--snapshot", "--clean")
+}
+
+func Release() error {
+	return sh.RunV("go", "run", fmt.Sprintf("github.com/goreleaser/goreleaser@%s", verGoReleaser), "release", "--clean")
 }
