83d1bf7a5ea9337d1ce0488b7bdd1fc670ec02f7
Anuraag Agrawal <anuraaga@gmail.com>
2024-01-11 16:09:47 +0900

    Add task to automatically update upstream version

--- magefiles/magefile.go
+++ magefiles/magefile.go
@@ -1,8 +1,15 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
 
+	"github.com/cli/go-gh/v2/pkg/api"
+	"github.com/google/go-github/v58/github"
+	"github.com/magefile/mage/mg"
 	"github.com/magefile/mage/sh"
 
 	"github.com/wasilibs/magefiles" // mage:import
@@ -18,4 +25,41 @@ func Snapshot() error {
 
 func Release() error {
 	return sh.RunV("go", "run", fmt.Sprintf("github.com/goreleaser/goreleaser@%s", verGoReleaser), "release", "--clean")
+}
+
+func UpdateUpstream() error {
+	currBytes, err := os.ReadFile(filepath.Join("buildtools", "wasm", "version.txt"))
+	if err != nil {
+		return err
+	}
+	curr := strings.TrimSpace(string(currBytes))
+
+	gh, err := api.DefaultRESTClient()
+	if err != nil {
+		return err
+	}
+
+	var releases []*github.RepositoryRelease
+	if err := gh.Get("repos/protocolbuffers/protobuf/releases?per_page=1", &releases); err != nil {
+		return err
+	}
+
+	if len(releases) == 0 {
+		return errors.New("could not find releases")
+	}
+
+	latest := releases[0].GetTagName()
+	if latest == curr {
+		fmt.Println("up to date")
+		return nil
+	}
+
+	fmt.Println("updating to", latest)
+	if err := os.WriteFile(filepath.Join("buildtools", "wasm", "version.txt"), []byte(latest), 0o644); err != nil {
+		return err
+	}
+
+	mg.Deps(magefiles.UpdateLibs)
+
+	return nil
 }
