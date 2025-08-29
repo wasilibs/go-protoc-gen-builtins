660493a4137a00e84413850e2a3a3752218ae4ec
Anuraag Agrawal <anuraaga@gmail.com>
2025-08-29 10:41:51 +0900

    Fix test

--- protoc_gen_builtins_test.go
+++ protoc_gen_builtins_test.go
@@ -81,7 +81,7 @@ func TestBuf(t *testing.T) {
 					env[i] = "PATH=" + pathVal
 				}
 			}
-			cmd := exec.Command("go", "tool", "buf", "generate")
+			cmd := exec.Command("go", "run", "github.com/bufbuild/buf/cmd/buf@v1.57.0", "generate")
 			cmd.Stderr = &output
 			cmd.Stdout = &output
 			cmd.Env = env
