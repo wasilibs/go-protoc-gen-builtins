22f0fb15c199c21187adf893aa52efc123599ab8
Anuraag Agrawal <anuraaga@gmail.com>
2025-09-08 14:49:45 +0900

    Update workflows

--- build/main.go
+++ build/main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"github.com/curioswitch/go-build"
+	"github.com/goyek/goyek/v2"
 	"github.com/goyek/x/boot"
+	"github.com/goyek/x/cmd"
 	"github.com/wasilibs/tools/tasks"
 )
 
@@ -10,6 +13,14 @@ func main() {
 		LibraryName: "protoc",
 		LibraryRepo: "protocolbuffers/protobuf",
 		GoReleaser:  true,
+	})
+	runBuf := "go run github.com/bufbuild/buf/cmd/buf@" + verBuf
+	build.RegisterCommandDownloads(runBuf + " --version")
+	goyek.Define(goyek.Task{
+		Name: "example",
+		Action: func(a *goyek.A) {
+			cmd.Exec(a, runBuf+" generate", cmd.Dir("example"))
+		},
 	})
 	boot.Main()
 }
---
+++ build/versions.go
@@ -0,0 +1,4 @@
+package main
+
+// renovate: github.com/bufbuild/buf
+const verBuf = "v1.57.0"
